5f56944ac0369f4421ad466a9a34a1c797557595
jior <jior2008@gmail.com>
2014-05-28 20:31:12 +0800

    update zookeeper service

--- elasticsearch1/bin/service.bat
+++ elasticsearch1/bin/service.bat
@@ -176,7 +176,7 @@ set JVM_OPTS=%JAVA_OPTS: =;%
 if not "%ES_JAVA_OPTS%" == "" set JVM_ES_JAVA_OPTS=%ES_JAVA_OPTS: =#%
 if not "%ES_JAVA_OPTS%" == "" set JVM_OPTS=%JVM_OPTS%;%JVM_ES_JAVA_OPTS%
 
-if "%ES_START_TYPE%" == "" set ES_START_TYPE=manual
+if "%ES_START_TYPE%" == "" set ES_START_TYPE=auto
 if "%ES_STOP_TIMEOUT%" == "" set ES_STOP_TIMEOUT=0
 
 "%EXECUTABLE%" //IS//%SERVICE_ID% --Startup %ES_START_TYPE% --StopTimeout %ES_STOP_TIMEOUT% --StartClass org.elasticsearch.bootstrap.Elasticsearch --StopClass org.elasticsearch.bootstrap.Elasticsearch --StartMethod main --StopMethod close --Classpath "%ES_CLASSPATH%" --JvmSs %JVM_SS% --JvmMs %JVM_XMS% --JvmMx %JVM_XMX% --JvmOptions %JVM_OPTS% ++JvmOptions %ES_PARAMS% %LOG_OPTS% --PidFile "%SERVICE_ID%.pid" --DisplayName "Elasticsearch (%SERVICE_ID%)" --Description "Elasticsearch Windows Service" --Jvm "%JVM_DLL%" --StartMode jvm --StopMode jvm --StartPath "%ES_HOME%"
